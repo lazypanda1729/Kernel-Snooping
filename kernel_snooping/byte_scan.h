991bd81a0a5dedacb7449c37a839f7784dc1c7ef
JustaT3ch <91199890+JustaT3ch@users.noreply.github.com>
2021-09-22 16:10:25 +0300

    Delete kernel_snooping directory

--- kernel_snooping/byte_scan.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "main_stuff.h"
-
-
-DWORD64 get_PspSetCreateProcessNotifyRoutine(HANDLE device, DWORD64 PsSetCreateProcessNotifyRoutine_address);
-
-DWORD64 get_PspCreateProcessNotifyRoutine_arr(HANDLE device, DWORD64 PspSetCreateProcessNotifyRoutine_address);
